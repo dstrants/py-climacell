bef7baace8e0b4a59ae431b69ea0ffab87a7f262
Dimitrios Strantsalis <dstrants@gmail.com>
2020-10-02 17:39:52 +0300

    adds historical data endpoints

--- climacell/__init__.py
+++
@@ -1,4 +0,0 @@
-from climacell.api import ClimaCellApi
-from climacell.config import cnf
-from climacell.exceptions import OpenCageApiTypeError, ClimaAPIError, ClimaCaseNoLocationError
-from climacell.locator import Locator
--- climacell/api.py
+++ climacell/api.py
@@ -2,7 +2,7 @@ from typing import List, Optional
 import requests
 import pendulum
 
-from climacell import cnf
+from climacell.config import cnf
 from climacell import exceptions as e
 from climacell.locator import Locator as L
 
@@ -29,13 +29,12 @@ class ClimaCellApi:
             return coordinates
         raise e.ClimaCaseNoLocationError
 
-    def call(self, endpoint: str = "", query: dict = dict(), method: str = "GET") -> dict:
+    def call(self, endpoint: str = "", query: dict = dict(), method: str = "GET",
+             unit: bool = True) -> dict:
         """Abstract method that performs the calls."""
-        query.update({
-            'apikey': self.api_key, 'lat': self.coords[0], 'lon': self.coords[1],
-            'unit_system': self.unit_system
-            })
-
+        query.update({'apikey': self.api_key, 'lat': self.coords[0], 'lon': self.coords[1]})
+        if unit:
+            query['unit_system'] = self.unit_system
         resp = requests.request(
             url=self.base_url + endpoint,
             method=method,
@@ -68,7 +67,7 @@ class ClimaCellApi:
             :param end_time: The end time of the analysis. Max >= 360 after the `start_time`
             :param fields: List of strings. Selected fields from the data layers e.g. `humidity`.
         """
-        end_time = end_time or str(pendulum.now().add(minutes=360))
+        end_time = end_time or str(pendulum.parse(start_time).add(minutes=360))
         query = {
             "start_time": start_time,
             "end_time": end_time,
@@ -87,7 +86,7 @@ class ClimaCellApi:
             :param end_time: The end time of the analysis. Max >= 108 hours after the `start_time`
             :param fields: List of strings. Selected fields from the data layers e.g. `humidity`.
         """
-        end_time = end_time or str(pendulum.now().add(hours=108))
+        end_time = end_time or str(pendulum.parse(start_time).add(hours=108))
         query = {
             "start_time": start_time,
             "end_time": end_time,
@@ -104,10 +103,59 @@ class ClimaCellApi:
             :param end_time: The end time of the analysis. Max >= 15 days after the `start_time`
             :param fields: List of strings. Selected fields from the data layers e.g. `humidity`.
         """
-        end_time = end_time or str(pendulum.now().add(days=15).start_of("day"))
+        end_time = end_time or str(pendulum.parse(start_time).add(days=15).start_of("day"))
         query = {
             "start_time": start_time,
             "end_time": end_time,
             "fields": fields or self.fields
         }
         return self.call("weather/forecast/daily", query)
+
+    def climacell(self, start_time: Optional[str] = None, end_time: Optional[str] = "now",
+                  timestep: int = 5, fields: List[str] = list()) -> dict:
+        """
+            ClimaCell’s proprietary historical weather information is provided
+            up to 6 hours in the past.
+
+            :param timestep: The interval of the historical data in minutes (defaults to `5min`)
+            :param start_time: The start time of the analysis.
+            :param end_time: The end time of the analysis. Max >= 360 after the `start_time`
+            :param fields: List of strings. Selected fields from the data layers e.g. `humidity`.
+        """
+        start_time = start_time or str(pendulum.parse(end_time).subtract(minutes=360))
+        query = {
+            "start_time": start_time,
+            "end_time": end_time,
+            "timestep": timestep,
+            "fields": fields or self.fields
+        }
+        return self.call("weather/historical/climacell", query)
+
+    def station(self, start_time: Optional[str] = None, end_time: Optional[str] = "now",
+                fields: List[str] = list()) -> dict:
+        """
+            Historical weather station information is provided globally from 4 weeks
+            in the past to the present. United States METAR and additional stations are included.
+
+            :param start_time: The start time of the analysis.
+            :param end_time: The end time of the analysis. Max >= weeks hours after the `start_time`
+            :param fields: List of strings. Selected fields from the data layers e.g. `humidity`.
+        """
+        start_time = start_time or str(pendulum.parse(end_time).subtract(minutes=360))
+        fields = fields or self.fields
+        for field in ('sunrise', 'sunset', 'weather_code'):
+            if field in fields:
+                fields.remove(field)
+        query = {
+            "start_time": start_time,
+            "end_time": end_time,
+            "fields": fields
+        }
+        return self.call("weather/historical/station", query)
+
+    def fire_index(self):
+        """
+        The average wildfire risk for the exact location,
+        given average climate conditions in the past 20 years.
+        """
+        return self.call("insights/fire-index", unit=False)
